aa5e5122b355e010b6040d0b3aa91ff93c6efb71
Mo Lawson <moklawson@gmail.com>
2018-07-30 16:33:02 -0700

    Define all test classes dynamically
    With the Article class defined in the test file, it'll end up calling
    the `boolean_timestamp` whenever that file is required. Let's define the
    class as late in the game as we can to avoid opaque errors in test
    setup.

--- spec/boolean_timestamp_spec.rb
+++ spec/boolean_timestamp_spec.rb
@@ -24,25 +24,25 @@ RSpec.describe BooleanTimestamp do # rubocop:disable Metrics/BlockLength
     let(:now) { Time.now }
 
     before do
-      Article.create!(title: "Before", published_at: now - 100)
-      Article.create!(title: "On", published_at: now)
-      Article.create!(title: "After", published_at: now + 100)
-      Article.create!(title: "Nil")
+      default_klass.create!(title: "Before", published_at: now - 100)
+      default_klass.create!(title: "On", published_at: now)
+      default_klass.create!(title: "After", published_at: now + 100)
+      default_klass.create!(title: "Nil")
     end
 
-    after { Article.delete_all }
+    after { default_klass.delete_all }
 
     context "positive" do
       it "only includes records before or matching the current time" do
         Timecop.freeze(now) do
-          expect(Article.published.map(&:title)).to eq(%w[Before On])
+          expect(default_klass.published.map(&:title)).to eq(%w[Before On])
         end
       end
 
       context "negative" do
         it "only includes records with null values or after the current time" do
           Timecop.freeze(now) do
-            expect(Article.not_published.map(&:title)).to eq(%w[After Nil])
+            expect(default_klass.not_published.map(&:title)).to eq(%w[After Nil])
           end
         end
       end
@@ -51,77 +51,77 @@ RSpec.describe BooleanTimestamp do # rubocop:disable Metrics/BlockLength
 
   context "attribute reader" do
     it "returns true for values before current time" do
-      expect(Article.new(published_at: Time.current - 100).published).to eq(true)
+      expect(default_klass.new(published_at: Time.current - 100).published).to eq(true)
     end
 
     it "returns true for values matching current time" do
-      expect(Article.new(published_at: Time.current).published).to eq(true)
+      expect(default_klass.new(published_at: Time.current).published).to eq(true)
     end
 
     it "returns false for values after current time" do
-      expect(Article.new(published_at: Time.current + 100).published).to eq(false)
+      expect(default_klass.new(published_at: Time.current + 100).published).to eq(false)
     end
 
     it "returns false for nil values" do
-      expect(Article.new.published).to eq(false)
+      expect(default_klass.new.published).to eq(false)
     end
   end
 
   context "boolean attribute reader" do
     it "returns true for values before current time" do
-      expect(Article.new(published_at: Time.current - 100)).to be_published
+      expect(default_klass.new(published_at: Time.current - 100)).to be_published
     end
 
     it "returns true for values matching current time" do
-      expect(Article.new(published_at: Time.current)).to be_published
+      expect(default_klass.new(published_at: Time.current)).to be_published
     end
 
     it "returns false for values after current time" do
-      expect(Article.new(published_at: Time.current + 100)).not_to be_published
+      expect(default_klass.new(published_at: Time.current + 100)).not_to be_published
     end
 
     it "returns false for nil values" do
-      expect(Article.new).not_to be_published
+      expect(default_klass.new).not_to be_published
     end
   end
 
   context "attribute writer" do
     it "sets current timestamp with truthy-ish value" do
-      expect(Article.new(published: true)).to be_published
-      expect(Article.new(published: "true")).to be_published
-      expect(Article.new(published: "1")).to be_published
-      expect(Article.new(published: "asdf")).to be_published
+      expect(default_klass.new(published: true)).to be_published
+      expect(default_klass.new(published: "true")).to be_published
+      expect(default_klass.new(published: "1")).to be_published
+      expect(default_klass.new(published: "asdf")).to be_published
 
-      expect(Article.new(published: true).published_at).to be_within(1).of(Time.now)
+      expect(default_klass.new(published: true).published_at).to be_within(1).of(Time.now)
     end
 
     it "clears timestamp with falsey-ish value" do
-      expect(Article.new(published: false)).not_to be_published
-      expect(Article.new(published: "FALSE")).not_to be_published
-      expect(Article.new(published: 0)).not_to be_published
-      expect(Article.new(published: "0")).not_to be_published
-      expect(Article.new(published: "f")).not_to be_published
-      expect(Article.new(published: "off")).not_to be_published
+      expect(default_klass.new(published: false)).not_to be_published
+      expect(default_klass.new(published: "FALSE")).not_to be_published
+      expect(default_klass.new(published: 0)).not_to be_published
+      expect(default_klass.new(published: "0")).not_to be_published
+      expect(default_klass.new(published: "f")).not_to be_published
+      expect(default_klass.new(published: "off")).not_to be_published
     end
 
     it "does not update timestamp with truthy-ish value if already present" do
-      post = Article.new(published_at: Time.current - 100)
+      post = default_klass.new(published_at: Time.current - 100)
       expect { post.published = true }.not_to(change { post.published_at })
     end
 
     it "works as expected when persisting and fetching from the database" do
-      post_id = Article.create!.id
-      expect(Article.find(post_id)).not_to be_published
+      post_id = default_klass.create!.id
+      expect(default_klass.find(post_id)).not_to be_published
 
-      Article.find(post_id).update!(published: true)
-      expect(Article.find(post_id)).to be_published
+      default_klass.find(post_id).update!(published: true)
+      expect(default_klass.find(post_id)).to be_published
 
-      expect { Article.find(post_id).update!(published: true) }
-        .not_to(change { Article.find(post_id).published_at })
-      expect(Article.find(post_id)).to be_published
+      expect { default_klass.find(post_id).update!(published: true) }
+        .not_to(change { default_klass.find(post_id).published_at })
+      expect(default_klass.find(post_id)).to be_published
 
-      expect { Article.find(post_id).update!(published: false) }
-        .to change { Article.find(post_id).published }.from(true).to(false)
+      expect { default_klass.find(post_id).update!(published: false) }
+        .to change { default_klass.find(post_id).published }.from(true).to(false)
     end
   end
 
@@ -136,10 +136,8 @@ RSpec.describe BooleanTimestamp do # rubocop:disable Metrics/BlockLength
       boolean_timestamp method_name
     end
   end
-end
 
-class Article < ActiveRecord::Base
-  include BooleanTimestamp
-
-  boolean_timestamp :published
+  def default_klass
+    @default_klass ||= define_attribute(:published)
+  end
 end

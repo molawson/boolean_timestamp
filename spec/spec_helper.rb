1fdd6d5f2aa558af3f884e8ece79eccfcf3929ed
Mo Lawson <mo@molawson.com>
2021-03-25 15:16:09 -0500

    Extract test database setup into a separate class
    Setup was becoming increasingly complicated and more difficult to trace
    and reason about when it was all dumped in spec_helper. As a separate
    class, we can hide the ugliness and take advantage of statefulness to
    consolidate logic, highlighting where we're actually doing something
    different for a different ActiveRecord version or database engine.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,71 +1,11 @@
 require "bundler/setup"
 require "boolean_timestamp"
 require "timecop"
-require "active_record/tasks/database_tasks"
+require_relative "support/test_database"
 
 if ENV["COVERAGE"]
   require "simplecov"
   SimpleCov.start
-end
-
-DB_ALREADY_EXISTS_ERROR = if ActiveRecord.const_defined?("DatabaseAlreadyExists")
-                            ActiveRecord::DatabaseAlreadyExists
-                          else
-                            ActiveRecord::Tasks::DatabaseAlreadyExists
-                          end
-
-
-def create_database_schema(config)
-  ActiveRecord::Base.establish_connection(config)
-  ActiveRecord::Schema.define do
-    suppress_messages do
-      create_table :articles do |t|
-        t.string :title
-        t.timestamp :published_at
-      end
-    end
-  end
-end
-
-def create_mysql_database(config)
-  ActiveRecord::Tasks::MySQLDatabaseTasks.new(db_config config).create
-rescue DB_ALREADY_EXISTS_ERROR
-  ActiveRecord::Tasks::MySQLDatabaseTasks.new(db_config config).drop
-  ActiveRecord::Tasks::MySQLDatabaseTasks.new(db_config config).create
-end
-
-def create_postgres_database(config)
-  ActiveRecord::Tasks::PostgreSQLDatabaseTasks.new(db_config config).create
-rescue DB_ALREADY_EXISTS_ERROR
-  ActiveRecord::Tasks::PostgreSQLDatabaseTasks.new(db_config config).drop
-  ActiveRecord::Tasks::PostgreSQLDatabaseTasks.new(db_config config).create
-end
-
-def setup_database(config)
-  case config["adapter"]
-  when "mysql2"
-    create_mysql_database(config)
-  when "postgresql"
-    create_postgres_database(config)
-  end
-  create_database_schema(config)
-end
-
-def teardown_database(config)
-  case config["adapter"]
-  when "mysql2"
-    ActiveRecord::Tasks::MySQLDatabaseTasks.new(db_config config).drop
-  when "postgresql"
-    ActiveRecord::Tasks::PostgreSQLDatabaseTasks.new(db_config config).drop
-  end
-end
-
-def db_config(config_hash)
-  if ActiveRecord.version >= Gem::Version.new("6.1.0")
-    ActiveRecord::DatabaseConfigurations::HashConfig.new("default_env", "primary", config_hash)
-  else
-    config_hash
-  end
 end
 
 RSpec.configure do |config|
@@ -76,34 +16,15 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  DB_CONFIG = {
-    "sqlite" => {
-      adapter: "sqlite3",
-      database: ":memory:",
-    },
-    "mysql" => {
-      "adapter" => "mysql2",
-      "host" => "127.0.0.1",
-      "username" => "root",
-      "password" => nil,
-      "database" => "boolean_timestamp_test",
-      "collation" => "utf8mb4_general_ci",
-    },
-    "postgres" => {
-      "adapter" => "postgresql",
-      "encoding" => "unicode",
-      "database" => "boolean_timestamp_test",
-    },
-  }.freeze
   adapter = ENV["DB_ADAPTER"] || "sqlite"
-  config_hash = DB_CONFIG.fetch(adapter)
+  test_database = TestDatabase.new(adapter)
 
   config.before(:suite) do
     puts "Running #{adapter} tests"
-    setup_database(config_hash)
+    test_database.setup
   end
 
   config.after(:suite) do
-    teardown_database(config_hash)
+    test_database.teardown
   end
 end
---
+++ spec/support/test_database.rb
@@ -0,0 +1,87 @@
+require "active_record/tasks/database_tasks"
+
+DB_CONFIG = {
+  "sqlite" => {
+    adapter: "sqlite3",
+    database: ":memory:",
+  },
+  "mysql" => {
+    "adapter" => "mysql2",
+    "host" => "127.0.0.1",
+    "username" => "root",
+    "password" => nil,
+    "database" => "boolean_timestamp_test",
+    "collation" => "utf8mb4_general_ci",
+  },
+  "postgres" => {
+    "adapter" => "postgresql",
+    "encoding" => "unicode",
+    "database" => "boolean_timestamp_test",
+  },
+}.freeze
+
+DB_ALREADY_EXISTS_ERROR = if ActiveRecord.const_defined?("DatabaseAlreadyExists")
+                            ActiveRecord::DatabaseAlreadyExists
+                          else
+                            ActiveRecord::Tasks::DatabaseAlreadyExists
+                          end
+
+class TestDatabase
+  def initialize(adapter)
+    @adapter = adapter.to_s
+  end
+
+  def setup
+    create
+    load_schema
+  end
+
+  def teardown
+    tasks.drop if tasks
+  end
+
+  private
+
+  attr_reader :adapter
+
+  def config
+    @config ||= begin
+                  config_hash = DB_CONFIG.fetch(adapter)
+                  if ActiveRecord.version >= Gem::Version.new("6.1.0")
+                    ActiveRecord::DatabaseConfigurations::HashConfig.new("default_env", "primary", config_hash)
+                  else
+                    config_hash
+                  end
+                end
+  end
+
+  def tasks
+    @tasks ||= case adapter
+               when "mysql"
+                 ActiveRecord::Tasks::MySQLDatabaseTasks.new(config)
+               when "postgres"
+                 ActiveRecord::Tasks::PostgreSQLDatabaseTasks.new(config)
+               end
+  end
+
+  def load_schema
+    ActiveRecord::Base.establish_connection(config)
+    ActiveRecord::Schema.define do
+      suppress_messages do
+        create_table :articles do |t|
+          t.string :title
+          t.timestamp :published_at
+        end
+      end
+    end
+  end
+
+  def create
+    return unless tasks
+
+    tasks.create
+  rescue DB_ALREADY_EXISTS_ERROR
+    tasks.drop
+    tasks.create
+  end
+end
